9654cdb8dc55959d50c75fd23458c156d56c1430
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-08-04 20:57:02 +0100

    establish db connection and add /users get

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const mysql = require("mysql");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const chalk = require("chalk");
@@ -7,14 +8,34 @@ const chalk = require("chalk");
 // Load environment variables from .env file, where API keys and passwords are configured.
 dotenv.config({path: ".env"});
 
+const db = mysql.createConnection(process.env.DB_CONNECTION_STRING);
+
+db.connect((err) => {
+	if (err) {
+		console.error(err);
+		console.log("%s MySQL connection error. Please make sure MySQL is running.", chalk.red("✗"));
+		process.exit();
+	}
+});
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-	res.json("Success");
+app.get("/users", (req, res) => {
+	try {
+		db.query("SELECT * FROM users", function(error, results) {
+			if (error) {
+				throw error;
+			}
+			res.json(results);
+		});
+	} catch (err) {
+		console.error(err);
+		res.json("Error: " + err);
+	}
 });
 
 app.listen(parseInt(process.env.PORT), () => {
