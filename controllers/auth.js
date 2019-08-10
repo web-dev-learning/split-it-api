9c3d57f2fd970b61d5be78925dd604c10a92471c
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-08-10 10:06:45 +0100

    validate and sanitise controllers inputs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,23 @@
 const db = require("../utils/db");
 
+const {checkRequiredFields} = require("../utils/validator");
+const {sanitiseEmail} = require("../utils/sanitiser");
+
 async function login(req, res) {
-	const {email, password} = req.body;
+	let {email, password} = req.body;
+
+	const missingFields = checkRequiredFields(["email", "password"], req.body);
+
+	if (missingFields.length > 0) {
+		res.statusCode = 400;
+		return res.json({
+			success: false,
+			message: `The following required fields are missing: ${missingFields.join(", ")}.`,
+			data: null
+		});
+	}
+
+	email = sanitiseEmail(email);
 
 	db.query("SELECT * FROM users WHERE email = ?", [email], (error, results, fields) => {
 		if (error) {
--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,41 @@
 const db = require("../utils/db");
+const {checkRequiredFields, isEmail, isMinLength} = require("../utils/validator");
+const {sanitiseEmail} = require("../utils/sanitiser");
 
 async function createNewUser(req, res) {
-	const {username, email, password} = req.body;
+	let {username, email, password} = req.body;
+
+	const missingFields = checkRequiredFields(["username", "email", "password"], req.body);
+
+	if (missingFields.length > 0) {
+		res.statusCode = 400;
+		return res.json({
+			success: false,
+			message: `The following required fields are missing: ${missingFields.join(", ")}.`,
+			data: null
+		});
+	}
+
+	if (!isEmail(email)) {
+		res.statusCode = 400;
+		return res.json({
+			success: false,
+			message: "The provided email is not valid.",
+			data: null
+		});
+	}
+
+	const minPasswordLength = 8;
+	if (!isMinLength(password, minPasswordLength)) {
+		res.statusCode = 400;
+		return res.json({
+			success: false,
+			message: `Password must be at least ${minPasswordLength} characters long.`,
+			data: null
+		});
+	}
+
+	email = sanitiseEmail(email);
 
 	db.query("INSERT INTO users SET ?", {username, email, password}, (error, results, fields) => {
 		if (error) {
