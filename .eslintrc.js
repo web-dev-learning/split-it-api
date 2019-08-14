9d84fdafc2dcdcbb92c9743c756eaffe58e96398
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-08-14 23:02:56 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+	rules: {},
+	parserOptions: {
+		ecmaVersion: 8
+	}
+};
