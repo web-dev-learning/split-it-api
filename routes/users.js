87ac7052ca43e7cd5b232982790c9efa3ab21cae
Cristi Ciobanu <c-ciobanu@protonmail.com>
2019-08-24 13:19:55 +0100

    add documentation

--- routes/users.js
+++ routes/users.js
@@ -34,6 +34,28 @@ const controller = require("../controllers/users");
  *     responses:
  *       200:
  *         $ref: '#/components/responses/Success'
+ *       400:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemasHidden/Response'
+ *             examples:
+ *               missingFields:
+ *                 $ref: '#/components/examples/MissingFields'
+ *               invalidEmail:
+ *                 summary: Email validation failure
+ *                 value:
+ *                   success: false
+ *                   message: The provided email is not valid.
+ *                   data: null
+ *               shortPassword:
+ *                 summary: Provided password is too short
+ *                 value:
+ *                   success: false
+ *                   message: Password must be at least ${min password length} characters long.
+ *                   data: null
+ *       500:
+ *         $ref: '#/components/responses/UnexpectedError'
  */
 router.route("/").post(controller.createNewUser);
 
